12285da92e8d13785b4cebe8bfb50e83d61b7c74
ajamroz <01158705@pw.edu.pl>
2021-10-25 10:08:30 +0200

    Working base for delicate improvements, plots and notes

--- LAB2/ewolucyjny.py
+++ LAB2/ewolucyjny.py
@@ -22,6 +22,8 @@
 # zbiory danych i/lub ich generatory należy samemu skonstruować na potrzebę zadania.
 
 import math
+import matplotlib.pyplot as plt
+import numpy as np
 from random import randint
 from random import randrange
 from random import sample
@@ -71,9 +73,7 @@ def generate_group_cities(neighbour_rad):       # 30 cities from definition
 
 
 def summary_distance(city_list):
-    mode_city_list = []
-    for element in city_list:
-        mode_city_list.append(element)
+    mode_city_list = city_list.copy()
     mode_city_list.append(mode_city_list[0])
     distance_sum = 0
     for i in range(len(mode_city_list)-1):
@@ -84,7 +84,9 @@ def summary_distance(city_list):
 def generate_population(members_quantity, city_list):
     population = []
     while len(population) < members_quantity:
-        population.append(sample(city_list, len(city_list)))
+        next_city = sample(city_list, len(city_list))
+        if next_city not in population:
+            population.append(next_city)
     return population
 
 
@@ -101,11 +103,14 @@ def turnee_select(population, func):
 
 
 def modify_element(modify_list):
-    first = randint(0, len(modify_list))
-    second = randint(0, len(modify_list))
-    temp = modify_list[first]
-    modify_list[first] = modify_list[second]
-    modify_list[second] = temp
+    if len(modify_list) > 0:
+        first = randint(0, len(modify_list)-2)
+        # second = randint(0, len(modify_list)-1)
+        second = first + 1
+        temp = modify_list[first]
+        modify_list[first] = modify_list[second]
+        modify_list[second] = temp
+    return modify_list
 
 
 def modify_population(population, mod_rate):
@@ -128,22 +133,31 @@ def turnee_winner_selection(comp_list, func):
     return winner
 
 
-def salesman_problem(cities_list):
-    population = generate_population(20, cities_list)
+def salesman_problem(cities_list, population_number, iterations, mod_rate):
+    population = generate_population(population_number, cities_list)
     i = 0
-    while i < 30:
-        new_temp_population = turnee_select(population, distance)
-        newer_temp_population = modify_population(new_temp_population, 0.2)
-        population = newer_temp_population
+    while i < iterations:
+        new_temp_population = turnee_select(population, summary_distance)
+        newer_temp_population = modify_population(new_temp_population, mod_rate)
+        population = newer_temp_population.copy()
         i += 1
-    winner = turnee_winner_selection(population, distance)
+    winner = turnee_winner_selection(population, summary_distance)
     return winner
 
+def draw(winner):
+    for i in range(0, len(winner)-1):
+        plt.plot([winner[i][0], winner[i+1][0]], [winner[i][1], winner[i+1][1]], 'ro-')
+    plt.plot([winner[-1][0], winner[0][0]], [winner[-1][1], winner[0][1]], 'ro-')
+    plt.title("Salesman route", fontsize=19)
+    plt.xlabel("Iks", fontsize=10)
+    plt.ylabel("Igrek", fontsize=10)
+    plt.tick_params(axis='both', which='major', labelsize=9)
+    plt.show()
 
-pop = generate_population(5, generate_random_cities(6))
-print("It's population: ")
-for element in pop:
-    print(element)
-print("It's turnee:")
-for elem in turnee_select(pop, summary_distance):
-    print(elem)
+random_cities = generate_chess_cities(7)
+random_cities = [[15, 55], [20, 70], [65, 55], [95, 10], [20, 5], [95, 45], [10, 45]]
+# random_cities = [[35, 85], [100, 35], [0, 65], [95, 10], [100, 50], [85, 25], [85, 95], [35, 95], [20, 45], [55, 45]]
+print("Random cities: ", random_cities)
+winner = salesman_problem(random_cities, 5000, 5000, 0)
+print("Best route: ", winner )
+draw(winner)
