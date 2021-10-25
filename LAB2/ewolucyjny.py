913a18fb6f6b6253929d9bacc8e678c5c6aa6bde
ajamroz <01158705@pw.edu.pl>
2021-10-25 10:35:00 +0200

    Final version with comments for extra slight changes and tests

--- LAB2/ewolucyjny.py
+++ LAB2/ewolucyjny.py
@@ -1,26 +1,3 @@
-# ALGORYTM EWOLUCYJNY:
-# - bez krzyżowania             +
-# - z selekcją turniejową       +
-# - z sukcesją generacyjną      +
-
-# Schemat algorytmu:
-# populacja bazowa ->                                                                                   +
-# selekcja (turniejowa) -> wszystkie osobniki przechodzą, 2n turnijów                                   +
-# mutacja -> nie każdy przechodzi, nie wszystkie wartości                                               +
-#            prawdopodobieństwo zajścia pewne, losuje se ze 2 miasta i zamienia miejscami np            +
-# sukcesja -> populacja po kombinacjach staje się populacją bazową                                      +
-
-# Sugerowane rozkłady miast sztuk 30:               +
-# jednorodny (taka szachownica)                     +
-# duże skupiska grup                                +
-# losowy                                            +
-# Wskaż sekwencję tych miast. (najkrótszy cykl)     +
-
-#  W raporcie należałoby wskazać
-# jak zmiana liczby osobników w populacji wpływa na jakość uzyskanych rozwiązań przy ograniczonym budżecie. Warto
-# również opisać zachowanie algorytmu dla różnych rodzajów danych wejściowych oraz wpływ zmiany parametrów. Przykładowe
-# zbiory danych i/lub ich generatory należy samemu skonstruować na potrzebę zadania.
-
 import math
 import matplotlib.pyplot as plt
 import numpy as np
@@ -30,16 +7,18 @@ from random import sample
 from random import choice
 
 
-CITY_QUANTITY = 30
+CITY_NUMBER = 30
 
 
+# distance between two chosen cities
 def distance(city_1, city_2):
     return math.sqrt((city_1[0] - city_2[0])**2 + (city_1[1] - city_2[1])**2)
 
 
-def generate_random_cities(city_quantity):
+# generating lists of cities
+def generate_random_cities(city_number):
     cities = []
-    while len(cities) < city_quantity:
+    while len(cities) < city_number:
         x = randint(0, 101)
         y = randint(0, 101)
         if [x,y] not in cities:
@@ -47,22 +26,22 @@ def generate_random_cities(city_quantity):
     return cities
 
 
-def generate_chess_cities(city_quantity):
+def generate_chess_cities(city_number):
     cities = []
-    while len(cities) < city_quantity:
-        x = randrange(0, 101, 5)
-        y = randrange(0, 101, 5)
+    while len(cities) < city_number:
+        x = randrange(0, 101, 10)
+        y = randrange(0, 101, 10)
         if [x,y] not in cities:
             cities.append([x,y])
     return cities
 
 
-def generate_group_cities(neighbour_rad):       # 30 cities from definition
-    main_cities = generate_random_cities(5)
+def generate_group_cities(groups, elements, neighbour_rad):       # 30 cities from definition
+    main_cities = generate_random_cities(groups)
     cities = []
     for city in main_cities:
         temporary_cities = []
-        while len(temporary_cities) < 6:
+        while len(temporary_cities) < elements:
             x = randint(city[0]-neighbour_rad, city[0]+neighbour_rad)
             y = randint(city[1]-neighbour_rad, city[1]+neighbour_rad)
             if [x,y] not in temporary_cities:
@@ -72,6 +51,7 @@ def generate_group_cities(neighbour_rad):       # 30 cities from definition
     return cities
 
 
+# summary distance between next cities from list
 def summary_distance(city_list):
     mode_city_list = city_list.copy()
     mode_city_list.append(mode_city_list[0])
@@ -81,15 +61,17 @@ def summary_distance(city_list):
     return distance_sum
 
 
-def generate_population(members_quantity, city_list):
+# population generation based on city list
+def generate_population(members_number, city_list):
     population = []
-    while len(population) < members_quantity:
+    while len(population) < members_number:
         next_city = sample(city_list, len(city_list))
         if next_city not in population:
             population.append(next_city)
     return population
 
 
+# tourney selection
 def turnee_select(population, func):
     new_population = []
     for i in range(len(population)):
@@ -102,6 +84,7 @@ def turnee_select(population, func):
     return new_population
 
 
+# modification of one element
 def modify_element(modify_list):
     if len(modify_list) > 0:
         first = randint(0, len(modify_list)-2)
@@ -113,6 +96,7 @@ def modify_element(modify_list):
     return modify_list
 
 
+# population modification
 def modify_population(population, mod_rate):
     number_to_be_modified = int(mod_rate * len(population))
     population_copy = population.copy()
@@ -125,6 +109,7 @@ def modify_population(population, mod_rate):
     return population_copy
 
 
+# selection of final winner
 def turnee_winner_selection(comp_list, func):
     winner = comp_list[0]
     for i in range(len(comp_list)):
@@ -133,17 +118,19 @@ def turnee_winner_selection(comp_list, func):
     return winner
 
 
+# final algorithm composition
 def salesman_problem(cities_list, population_number, iterations, mod_rate):
     population = generate_population(population_number, cities_list)
     i = 0
     while i < iterations:
         new_temp_population = turnee_select(population, summary_distance)
-        newer_temp_population = modify_population(new_temp_population, mod_rate)
-        population = newer_temp_population.copy()
+        population = modify_population(new_temp_population, mod_rate)
         i += 1
     winner = turnee_winner_selection(population, summary_distance)
     return winner
 
+
+# drawing a plot of cities
 def draw(winner):
     for i in range(0, len(winner)-1):
         plt.plot([winner[i][0], winner[i+1][0]], [winner[i][1], winner[i+1][1]], 'ro-')
@@ -154,10 +141,15 @@ def draw(winner):
     plt.tick_params(axis='both', which='major', labelsize=9)
     plt.show()
 
-random_cities = generate_chess_cities(7)
-random_cities = [[15, 55], [20, 70], [65, 55], [95, 10], [20, 5], [95, 45], [10, 45]]
+
+# TESTY:
+random_cities = generate_group_cities(3, 2, 4)
+# random_cities = [[15, 55], [20, 70], [65, 55], [95, 10], [20, 5], [95, 45], [10, 45]]
 # random_cities = [[35, 85], [100, 35], [0, 65], [95, 10], [100, 50], [85, 25], [85, 95], [35, 95], [20, 45], [55, 45]]
 print("Random cities: ", random_cities)
-winner = salesman_problem(random_cities, 5000, 5000, 0)
-print("Best route: ", winner )
-draw(winner)
+route = salesman_problem(random_cities, 700, 500, 0)
+print("Best route: ", route)
+draw(route)
+
+
+# można zmodyfikować sposób modyfikacji
