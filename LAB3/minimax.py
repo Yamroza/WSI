f12e41a9dc1abc3082297d5c90862510655763f2
Yamroza <oyamroz@gmail.com>
2021-11-28 16:20:46 +0100

    Added new heuristics

--- LAB3/minimax.py
+++ LAB3/minimax.py
@@ -1,14 +1,42 @@
+from copy import deepcopy
+
+
 def heuristic(field):
     if field.is_max:
-        return len(field.available_moves(field.is_max)) - len(field.available_moves(not field.is_max))
+        return len(field.available_moves(field.is_max)) 
     else: 
-        return len(field.available_moves(not field.is_max)) - len(field.available_moves(field.is_max))
+        return - len(field.available_moves(not field.is_max))
+
+
+def differ_heuristic(field):
+    one_moves = field.available_moves(True)
+    two_moves = field.available_moves(False)
+
+    if field.is_max:
+        return len(one_moves) - len(two_moves)
+    else: 
+        return - (len(two_moves) - len(one_moves))
+
+
+def off_heuristic(field):
+    if field.is_max:
+        return len(field.available_moves(field.is_max)) - 2 * len(field.available_moves(not field.is_max))
+    else: 
+        return - (len(field.available_moves(not field.is_max)) - 2 * len(field.available_moves(field.is_max)))
+
+
+def deff_heuristic(field):
+    if field.is_max:
+        return 2 * len(field.available_moves(field.is_max)) - len(field.available_moves(not field.is_max))
+    else: 
+        return - (2 * len(field.available_moves(not field.is_max)) - len(field.available_moves(field.is_max)))
+
 
 def minimax_basic(field, depth):
     is_max = field.is_max
     ist = field.is_winner()
     if depth == 0:
-        return heuristic(field)
+        return differ_heuristic(field)
     elif ist:
         if field.winner:
             return 1
