c86384e0210f01d59cd74db32a7f1dcd08ed4e02
Yamroza <oyamroz@gmail.com>
2021-12-29 17:01:27 +0100

    Project for testing

--- LAB6/q_uber.py
+++ LAB6/q_uber.py
@@ -57,19 +57,8 @@ class Musk_Taxi:
         self.x_done = self.rows - 1
         self.y_done = self.rows - 1
         self.done = False
-        self.available_moves = self.check_available_moves()
+        # self.available_moves = self.check_available_moves()
         
-
-    def check_available_moves(self):
-        available_moves = []
-        for y, x in zip([-1, 1, 0, 0], [0, 0, -1, 1]):
-            if self.x + x > -1 and self.x + x < self.rows:
-                if self.y + y > -1 and self.y + y < self.rows:
-                    if self.lab[self.x + x][self.y + y] == 0:
-                        available_moves.append([self.y+y,self.x+x])
-        return available_moves
-
-
     def make_action(self, action):
         previous_position = self.x, self.y
         if action == 0: 
@@ -92,8 +81,20 @@ class Musk_Taxi:
         else:
             return -1
 
+    def drive(self, q_table, beta, gamma, epsilon):
+        steps = []
+        while not self.done:
+            state = self.get_state()
+            if uniform(0,1) < epsilon:
+                action = randint(0,3)
+            else:
+                action = np.argmax(q_table[state])
+            steps.append(action)
+            reward = self.make_action(action)
+            next_state = self.get_state()
+            q_table = update_q_table(q_table, state, action, beta, gamma, reward, next_state)
+        return steps, q_table
             
-
     def is_done(self):
         if self.y == self.y_done and self.x == self.x_done:
             return True
@@ -109,74 +110,82 @@ class Musk_Taxi:
         return state
 
 
-class Random_car:
-    def __init__(self, musk_taxi = Musk_Taxi(5, 3)):
-        self.taxi = musk_taxi
+# class Random_car(Musk_Taxi):
+#     def __init__(self, musk_taxi = Musk_Taxi(8, 3)):
+#         self.taxi = musk_taxi
 
-    def steps(self):
-        steps = 0
-        while not self.taxi.is_done():
-            move = choice(self.taxi.check_available_moves())
-            self.taxi.y, self.taxi.x = move
-            steps += 1
-        return steps
+#     def steps(self):
+#         steps = 0
+#         while not self.taxi.is_done():
+#             move = choice(self.taxi.check_available_moves())
+#             self.taxi.y, self.taxi.x = move
+#             steps += 1
+#         return steps
+
+#     def check_available_moves(self):
+#         available_moves = []
+#         for y, x in zip([-1, 1, 0, 0], [0, 0, -1, 1]):
+#             if self.taxi.x + x > -1 and self.taxi.x + x < self.rows:
+#                 if self.taxi.y + y > -1 and self.taxi.y + y < self.rows:
+#                     if self.lab[self.taxi.x + x][self.taxi.y + y] == 0:
+#                         available_moves.append([self.taxi.y+y,self.taxi.x+x])
+#         return available_moves
 
 
 def generate_q_table(columns, rows):
     return np.zeros((columns, rows))
 
-def update_q_table(q_table, state, action, beta, gamma, reward, new_state):
-    new_state_max = np.max(q_table[new_state])
-    q_table[state, action] = (1-beta) * q_table[state, action] + beta * (reward + gamma * new_state_max - q_table[state, action])
+def update_q_table(q_table, state, action, beta, gamma, reward, next_state):
+    next_state_max = np.max(q_table[next_state])
+    # q_table[state, action] = (1-beta) * q_table[state, action] + beta * (reward + gamma * next_state_max - q_table[state, action])
+    q_table[state, action] += beta * (reward + gamma * next_state_max - q_table[state, action])
+
+    return q_table
     
 
 # TESTS:
 def main():
 
     # parameters:
-    epsilon = 0.1
-    gamma = 0.6
-    beta = 0.1      #  learning rate
+    # epsilon = 0.1
+    # gamma = 0.6
+    # beta = 0.1      #  learning rate
+    
+    data = open('taxi_data.txt', 'a')
+    # line = "epsilon;gamma;beta;iterations"
+    # data.write(line)
+    expected_steps = [3, 3, 3, 0, 3, 3, 0, 3, 3, 0, 0, 2, 2, 0, 2, 2, 2, 0, 0, 3, 3, 3, 3, 3]
+    for epsilon in [0.15, 0.3]:
+        for gamma in [0.4, 0.6, 0.8]:
+            for beta in [0.1, 0.3, 0.5]:
+                steps = []
+                q_table = generate_q_table(64, 4)
+                iteration_no = 0
+                while steps != expected_steps:
+                    taxi = Musk_Taxi(8, 5)
+                    steps = []
+                    steps, q_table = taxi.drive(q_table, beta, gamma, epsilon)
+                    iteration_no += 1
+                line = ("\n" + str(epsilon) + ";" + str(gamma) + ";" + str(beta) + ";" + str(iteration_no))
+                data.write(line)
+    data.close()
 
-    taxi = Musk_Taxi(5, 5)
-    q_table = generate_q_table(taxi.no_of_states(), 4)
+    # interval_steps = []
+    # for i in range(10000):
+    #     steps = []
+    #     taxi = Musk_Taxi(8, 5)
+    #     steps, q_table = taxi.drive(q_table, beta, gamma, epsilon)
+    #     if i % 20 == 0:
+    #         interval_steps.append(steps)
 
-    interval_steps = []
-    for i in range(100):
-        steps = []
-        taxi = Musk_Taxi(5, 5)
-        while not taxi.done:
-            state = taxi.get_state()
-            if uniform(0,1) < epsilon:
-                action = randint(0,3)
-            else:
-                action = np.argmax(q_table[state])
-            if i % 20 == 0:
-                steps.append(action)
-            reward = taxi.make_action(action)
-            new_state = taxi.get_state()
-            update_q_table(q_table, state, action, beta, gamma, reward, new_state)
-        if i % 20 == 0:
-            interval_steps.append(steps)
+    # print(interval_steps[-1])
 
-    print(interval_steps[-1])
-    # taxi = Musk_Taxi(5, 5)
-    # for row in taxi.lab:
-    #     print(row)
-    # steps = 0
-    # while not taxi.done:
-    #     state = taxi.get_state()
-    #     if uniform(0,1) < epsilon:
-    #         action = randint(0,3)
-    #     else:
-    #         action = np.argmax(q_table[state])
-    #     reward = taxi.make_action(action)
-    #     new_state = taxi.get_state()
-    #     update_q_table(q_table, state, action, beta, gamma, reward, new_state)
-    #     steps += 1
+
+
+    # lab = generate_lab(8, 20)
+    # save_lab_to_file(lab, 'LAB6/saved_lab.txt')
     
 
-    # print(steps)
 
 
     # random_car = Random_car()
--- LAB6/uber_visualise.py
+++ LAB6/uber_visualise.py
@@ -1,6 +1,8 @@
 import pygame
 from random import randint
+from PIL import Image
 from q_uber import Musk_Taxi
+import os
 
 LIGHT_BLUE = (50, 185, 250)
 DARK_BLUE = (45, 145, 180)
@@ -8,9 +10,10 @@ ORANGE = (255, 180, 35)
 GREEN = (45, 200, 35)
 BLACK = (0,0,0)
 RED = (255, 0, 0)
+GRAY = (80,80,80)
 
 WIDTH = HEIGHT = 840
-FPS = 1
+FPS = 5
 
 pygame.init()
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -22,25 +25,31 @@ class Game:
         self.win = win
         self.taxi = taxi
         self.size = HEIGHT/self.taxi.rows
-        self.steps = [0, 0, 3, 3, 0, 0, 3, 3]
+        self.steps = [3, 3, 3, 0, 3, 3, 0, 3, 3, 0, 0, 2, 2, 0, 2, 2, 2, 0, 0, 3, 3, 3, 3, 3]
         self.iter = 0
-        self.image = pygame.image.load('LAB6/car1.jpg')
+        image = Image.open('LAB6/car.jpg')
+        new_image = image.resize((int(self.size), int(self.size)))
+        new_image.save('LAB6/car12.jpg')
+        self.car_image = pygame.image.load('LAB6/car12.jpg')
+        image = Image.open('LAB6/lantern.jpg')
+        new_image = image.resize((int(self.size*0.94), int(self.size*0.94)))
+        new_image.save('LAB6/lantern12.jpg')
+        self.lantern_image = pygame.image.load('LAB6/lantern12.jpg')
 
     def update(self):
-        self.win.fill(LIGHT_BLUE)
+        self.win.fill(BLACK)
         for row in range(0, self.taxi.rows):
-            for col in range(row % 2, self.taxi.rows, 2):
-                pygame.draw.rect(self.win, DARK_BLUE, (col * self.size, row * self.size, self.size, self.size))
+            for col in range(0, self.taxi.rows):
+                pygame.draw.rect(self.win, LIGHT_BLUE, ((col+0.03) * self.size, (row+0.03) * self.size, self.size*0.94, self.size*0.94))
 
         for x in range(self.taxi.rows):
             for y in range(self.taxi.rows):
                 if self.taxi.lab[x][y] == 1:
-                    pygame.draw.rect(self.win, RED, (y * self.size, x * self.size, self.size, self.size))
-                    pygame.draw.rect(self.win, BLACK, ((y + 0.03)* self.size, (x + 0.03) * self.size, self.size * 0.95, self.size * 0.95))
+                    self.win.blit(self.lantern_image, ((y+0.03) * self.size, (x+0.03) * self.size))
 
-        self.win.blit(self.image, (self.taxi.y * self.size, self.taxi.x * self.size))
-        # pygame.draw.circle(self.win, BLACK, ((self.taxi.y + 0.5) * self.size , (self.taxi.x + 0.5) * self.size), self.size/2.8)
-        # pygame.draw.circle(self.win, GREEN, ((self.taxi.y + 0.5) * self.size , (self.taxi.x + 0.5) * self.size), self.size/3)
+        self.win.blit(self.car_image, (self.taxi.y * self.size, self.taxi.x * self.size))
+        pygame.draw.circle(self.win, BLACK, ((self.taxi.y_done + 0.5) * self.size , (self.taxi.x_done + 0.5) * self.size), self.size/2.8)
+        pygame.draw.circle(self.win, RED, ((self.taxi.y_done + 0.5) * self.size , (self.taxi.x_done + 0.5) * self.size), self.size/3)
 
         self.taxi.make_action(self.steps[self.iter])
         if self.iter < len(self.steps) - 1:
@@ -51,7 +60,7 @@ def main():
     global FPS
     run = True
     clock = pygame.time.Clock()
-    taxi = Musk_Taxi(5, 10)
+    taxi = Musk_Taxi(8, 10)
     game = Game(WIN, taxi)
     while run:
         clock.tick(FPS)
@@ -61,6 +70,8 @@ def main():
         game.update()
         pygame.display.update()
     pygame.quit()
+    os.remove('LAB6/car12.jpg')
+    os.remove('LAB6/lantern12.jpg')
 
 
 if __name__ == "__main__":
